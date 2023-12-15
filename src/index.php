80e9da726ca20f2b40ee7f1f1a344c6a369a0b0e
Louise <Louise.blanc@chasacademy.se>
2023-12-15 15:16:32 +0100

    "Form basic structure"

--- src/index.php
+++ src/index.php
@@ -1,2 +1,27 @@
 <!-- kolla tidighare php-pdo uppgifter för skapa bra struktur och få inspo -->
+<?php 
+require 'db_conn.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <main class="main-section">
+        <section class="add-section">
+            <form action="">
+                <input type="text" 
+                       name="title" 
+                       placeholder="required">
+            </form>
+        </section>
+    </main>
 
+</body>
+</html>
